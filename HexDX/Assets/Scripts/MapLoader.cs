7c08733c65f05177a2c45a00f344ec3582dfe939
Anurag Papineni <anuragpapineni@gmail.com>
2016-09-02 16:48:40 -0500

    Added some simple code to generate a map from a csv file. Values need to be toyed with a little. Probably want globals for tile size. Also created basic tile class

---
+++ HexDX/Assets/Scripts/MapLoader.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class MapLoader : MonoBehaviour {
+    public List<List<Tile>> map = new List<List<Tile>>();
+    public List<Sprite> sprites;
+
+    // Use this for initialization
+    void Start () {
+        var reader = new StreamReader(File.OpenRead("Assets/Maps/test.csv"));
+        List<Tile> row;
+        float x = -5;
+        float y = 5;
+        Sprite sprite = null;
+        while (!reader.EndOfStream)
+        {
+            string[] line = reader.ReadLine().Split(',');
+            row = new List<Tile>();
+            foreach (string num in line) {
+                GameObject tile = new GameObject();
+                Tile t = tile.AddComponent<Tile>();
+                tile.AddComponent<SpriteRenderer>();
+                sprite = sprites[int.Parse(num)];
+                t.MakeTile(int.Parse(num),sprite);
+                tile.transform.position = new Vector3(x,y,0);
+                row.Add(t);
+                x += 2*sprite.bounds.extents.x;
+            }
+            map.Add(row);
+            y -= 1.5f*sprite.bounds.extents.y;
+            x -= 2 * sprite.bounds.extents.x*line.Length+sprite.bounds.extents.x;
+
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ HexDX/Assets/Scripts/Tile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour{
+    protected int type;
+    protected Sprite sprite;
+
+    public void MakeTile(int type, Sprite sprite)
+    {
+        this.type = type;
+        this.gameObject.GetComponent < SpriteRenderer >().sprite = sprite;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
