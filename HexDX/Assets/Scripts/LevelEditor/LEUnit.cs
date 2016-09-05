23899916c12ce04505931c7bb30a349787b35f62
ZackMisso <zack441@mac.com>
2016-09-05 15:40:31 -0700

    Added Files Needed for Level Editor

--- HexDX/Assets/Scripts/LevelEditor/LEUnit.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class LEUnit : MonoBehaviour {
-    // to be implemented
-}
--- HexDX/Assets/Scripts/LevelEditor/LEUnitButton.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class LEUnitButton : MonoBehaviour {
-    // to be implemented
-}
