d02f9371b152b835c97ea5e2dc98d6a219f6ff5f
ZackMisso <zack441@mac.com>
2016-09-19 20:08:22 -0700

    Moved Editor Sprites into the Correct Folder

--- HexDX/Assets/Scripts/LevelEditor/LEUnitBar.cs
+++ HexDX/Assets/Scripts/LevelEditor/LEUnitBar.cs
@@ -2,17 +2,6 @@
 using System.Collections;
 
 public class LEUnitBar : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public void MoveUp()
     {
         // to be implemented
