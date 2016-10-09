b5a8a2915a2c9cf02fd89ac46ae18d0a0247a20c
ZackMisso <zack441@mac.com>
2016-10-09 13:20:01 -0700

    Map Formats and Loader Updated to Support Units

--- HexDX/Assets/Scripts/LevelEditor/LEMapLoader.cs
+++ HexDX/Assets/Scripts/LevelEditor/LEMapLoader.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -13,18 +14,27 @@ public class LEMapLoader : MonoBehaviour {
 
         var reader = new StreamReader(File.OpenRead(fileName));
 
-        int i = 0;
+        //int i = 0;
         float x = 0.0f;
         float y = 0.0f;
         float z = 0.0f;
 
-        while (!reader.EndOfStream)
+        string[] mapDim = reader.ReadLine().Split(',');
+
+        if (mapDim.Length != 2) Debug.Log("LEVEL FORMAT ERROR :: NO DIMENSIONS FOUND");
+
+        int hei = Convert.ToInt32(mapDim[0]);
+        int wid = Convert.ToInt32(mapDim[1]);
+
+        //Debug.Log("HEI :: "+hei+" WID :: "+wid);
+
+        for(int i=0;i<hei;i++)
         {
             string[] line = reader.ReadLine().Split(',');
 
             hexMap.AppendRow();
 
-            for (int j=0;j<line.Length;j++)
+            for (int j=0;j<wid;j++)
             {
                 Vector3 pos = new Vector3(x, y, z);
                 GameObject newTile = hexMap.CreateTileObject(pos, i, j);
@@ -38,9 +48,12 @@ public class LEMapLoader : MonoBehaviour {
             x -= 2 * hexMap.hexDimension.width * line.Length - hexMap.hexDimension.width;
             z -= .001f;
 
-            Debug.Log(hexMap.hexDimension.width);
-
-            i++;
+            //Debug.Log(hexMap.hexDimension.width);
         }
+
+        int numUnits = Convert.ToInt32(reader.ReadLine());
+        //Debug.Log("NUM UNITS :: " + numUnits);
+
+        reader.Close();
     }
 }
