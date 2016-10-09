5786ad406127d3f09f03b356dc61fe0ba8f82d4a
Anurag Papineni <anuragpapineni@gmail.com>
2016-10-09 18:59:44 -0500

    basic architecture for world map

---
+++ HexDX/Assets/Scripts/GameManagement/Territory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Territory : MonoBehaviour {
+    public List<Territory> neighbors;
+    public LevelManager lm;
+    public bool captured;
+
+    public void Start()
+    {
+        this.GetComponent<SpriteRenderer>().color = Color.gray;
+    }
+    public void OnMouseEnter()
+    {
+        this.GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    public void OnMouseExit()
+    {
+        this.GetComponent<SpriteRenderer>().color = Color.gray;
+    }
+
+    public void OnMouseDown()
+    {
+        lm.StartLevel();
+    }
+}
