38a45962720cc98a772e7d9db9ba73f9f09c93b8
Anurag Papineni <anuragpapineni@gmail.com>
2016-10-25 22:59:02 -0500

    UI changes so that everything scales with screen size

---
+++ HexDX/Assets/Scripts/UI/BattleMap/AspectRatioScaler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AspectRatioScaler : MonoBehaviour {
+    float originalratio = 16.0f / 9.0f;
+    float currentAspect;
+    float origx, origy;
+
+	// Use this for initialization
+	void Start () {
+        float ratio = Camera.main.aspect;
+        origx = transform.localScale.x;
+        origy = transform.localScale.y;
+        currentAspect = ratio;
+        transform.localScale = new Vector3(origx*ratio/originalratio, origy, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Camera.main.aspect != currentAspect)
+        {
+            float ratio = Camera.main.aspect;
+            currentAspect = ratio;
+            transform.localScale = new Vector3(origx * ratio / originalratio, origy, 1);
+        }
+    }
+}
