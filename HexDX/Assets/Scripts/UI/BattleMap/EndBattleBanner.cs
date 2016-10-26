cc42e2bb8e6e369e29fc6d48ac921f528547d6eb
Alissa Hall <alissa.hall@utexas.edu>
2016-10-26 11:20:40 -0500

    added box to display money won at end of battle

--- HexDX/Assets/Scripts/UI/BattleMap/EndBattleBanner.cs
+++ HexDX/Assets/Scripts/UI/BattleMap/EndBattleBanner.cs
@@ -5,10 +5,12 @@ using System.Collections.Generic;
 public class EndBattleBanner : MonoBehaviour {
 	public GameObject winBanner;
 	public GameObject lossBanner;
+	public GameObject winningsBox;
 
 	void Awake() {
 		winBanner = transform.Find("WinBanner").gameObject;
 		lossBanner = transform.Find("LossBanner").gameObject;
+		winningsBox = transform.Find("WinningsBox").gameObject;
 	}
 
 	void Start() {
@@ -17,7 +19,14 @@ public class EndBattleBanner : MonoBehaviour {
 
 	public void ShowWin() {
 		Debug.Log("Player Wins!");
+		LevelManager lm = FindObjectOfType(typeof(LevelManager)) as LevelManager;
+		if (lm) {
+			winningsBox.GetComponentInChildren<Text>().text = "You won " + lm.moneyRewarded + " gold!";
+		} else {
+			winningsBox.GetComponentInChildren<Text>().text = "You won 1000 gold!";
+		}
 		winBanner.SetActive(true);
+		winningsBox.SetActive(true);
 	}
 
 	public void ShowLoss() {
@@ -28,5 +37,6 @@ public class EndBattleBanner : MonoBehaviour {
 	private void Hide() {
 		winBanner.SetActive(false);
 		lossBanner.SetActive(false);
+		winningsBox.SetActive(false);
 	}
 }
