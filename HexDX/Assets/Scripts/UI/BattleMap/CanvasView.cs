62488b657f586913eb3a23df7496c2f99cdac6a8
ZackMisso <zack441@mac.com>
2016-10-30 13:16:37 -0700

    Added Script to Hide All UI when 'h' is pressed

---
+++ HexDX/Assets/Scripts/UI/BattleMap/CanvasView.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CanvasView : MonoBehaviour {
+    private List<Canvas> canvases; // set in ui
+    private bool enabled;
+
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            Canvas canvas = child.gameObject.GetComponent<Canvas>();
+            if (canvas != null) canvases.Add(canvas);
+        }
+        enabled = true;
+    }
+	
+	void Update () {
+	    if (Input.GetKeyDown("h"))
+        {
+            if (enabled) DisableCanvas();
+            else EnableCanvas();
+            enabled = !enabled;
+        }
+	}
+
+    private void EnableCanvas()
+    {
+        foreach (Canvas canvas in canvases)
+        {
+            canvas.enabled = true;
+        }
+    }
+
+    private void DisableCanvas()
+    {
+        foreach (Canvas canvas in canvases)
+        {
+            canvas.enabled = false;
+        }
+    }
+}
