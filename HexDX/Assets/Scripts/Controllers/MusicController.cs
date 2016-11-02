74479fc93da531deed3fa4a89f2a52d9e3e0db58
Anurag Papineni <anuragpapineni@gmail.com>
2016-11-02 16:14:32 -0500

    music changes and added turn transitions to tutorial

--- HexDX/Assets/Scripts/Controllers/MusicController.cs
+++ HexDX/Assets/Scripts/Controllers/MusicController.cs
@@ -2,13 +2,76 @@
 using System.Collections;
 
 public class MusicController : MonoBehaviour {
-
+    public AudioSource audio;
+    public AudioClip victory;
+    public AudioClip steampunk;
+    public AudioClip medieval;
 	// Use this for initialization
 	void Start () {
+        audio = GetComponent<AudioSource>();
+        setSteamPunk();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void setSteamPunk()
+    {
+        audio.clip = steampunk;
+        audio.Play();
+        audio.loop = true;
+    }
+
+    public void setMedieval()
+    {
+        audio.clip = medieval;
+        audio.Play();
+        audio.loop = true;
+    }
+
+    public void setVictory()
+    {
+        audio.clip = victory;
+        audio.Play();
+        audio.volume = 1.0f;
+        audio.loop = false;
+    }
+    // Update is called once per frame
+    void Update () {
+        if (BattleController.PlayerWon)
+        {
+            if (audio.clip != victory)
+            {
+                audio.volume -= .01f;
+                if (audio.volume < .1f)
+                    setVictory();
+            }
+        }
+        else if (BattleController.IsPlayerTurn)
+        {
+            if (audio.clip == medieval)
+            {
+                audio.volume -= .01f;
+                if (audio.volume < .1f)
+                    setSteamPunk();
+            }
+            else if (audio.clip == steampunk)
+            {
+                if (audio.volume < 1.0f)
+                    audio.volume += .01f;
+            }
+        }
+        else
+        {
+            if (audio.clip == steampunk)
+            {
+                audio.volume -= .01f;
+                if (audio.volume < .1f)
+                    setMedieval();
+            }
+            else if (audio.clip == medieval)
+            {
+                if (audio.volume < 1.0f)
+                    audio.volume += .01f;
+            }
+        }
+
+    }
 }
