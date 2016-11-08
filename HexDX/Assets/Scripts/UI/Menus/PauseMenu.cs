8e7a6d514bc9c15fffd0d61520f5a4db57ff4319
Alissa Hall <alissa.hall@utexas.edu>
2016-11-08 17:19:55 -0600

    fixed issue where pause menu did not reactivate units on return to world map

--- HexDX/Assets/Scripts/UI/Menus/PauseMenu.cs
+++ HexDX/Assets/Scripts/UI/Menus/PauseMenu.cs
@@ -1,68 +1,57 @@
 ﻿using System;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
-{
-    bool paused = false;
-    SelectionMode lastMode;
+public class PauseMenu : MonoBehaviour {
+    private bool paused = false;
+    private SelectionMode lastMode;
 
     void Update() {
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            paused = true;
-            GetComponent<CameraController>().enabled = false;
-            Pause();
-        }
-         
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (paused) {
+                Unpause();
+            } else {
+                Pause();
+            }
+        }       
     }
 
-    void Start()
-    {
-    }
-    void OnGUI()
-    {
-        if (paused)
-        {
+    void OnGUI() {
+        if (paused) {
             //Make a background box
             GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 250, 200), "Pause Menu");
 
             //Make Main Menu button
-            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 250, 50), "Main Menu"))
-            {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 250, 50), "Main Menu")) {
                 LevelManager.ReturnToMainMenu();
             }
-            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2, 250, 50), "Return To WorldMap"))
-            {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2, 250, 50), "Return To World Map")) {
+                Unpause();
                 LevelManager.ReturnToWorldMap();
             }
 
             //Make Change Graphics Quality button
-            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2+50, 250, 50), "Unpause"))
-            {
-                GetComponent<CameraController>().enabled = true;
-                paused = false;
-                UnPause();
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2+50, 250, 50), "Unpause")) {
+                Unpause();
             }
         }
     }
 
-    void Pause()
-    {
+    void Pause() {
+        paused = true;
+        GetComponent<CameraController>().enabled = false;
         Unit[] allUnits = FindObjectsOfType(typeof(Unit)) as Unit[];
-        foreach (Unit unit in allUnits)
-        {
+        foreach (Unit unit in allUnits) {
             unit.enabled = false;
             unit.gameObject.GetComponent<Animator>().enabled = false;
             unit.gameObject.GetComponent<AudioSource>().enabled = false;
         }
     }
 
-    void UnPause()
-    {
+    void Unpause() {
+        paused = false;
+        GetComponent<CameraController>().enabled = true;
         Unit[] allUnits = FindObjectsOfType(typeof(Unit)) as Unit[];
-        foreach (Unit unit in allUnits)
-        {
+        foreach (Unit unit in allUnits) {
             unit.enabled = true;
             unit.gameObject.GetComponent<Animator>().enabled = true;
             unit.gameObject.GetComponent<AudioSource>().enabled = true;
