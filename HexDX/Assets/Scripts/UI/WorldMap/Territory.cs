95eaeb636f0b96f648ce6b3bbaf828a1f8eb9c7d
Alissa Hall <alissa.hall@utexas.edu>
2016-12-07 21:04:33 -0600

    changed captured territory color to red

--- HexDX/Assets/Scripts/UI/WorldMap/Territory.cs
+++ HexDX/Assets/Scripts/UI/WorldMap/Territory.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 // implements IPointer interfaces so will be properly inactive when UI elemnents overlap it
 public class Territory : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler {
     private static readonly Color hoverColor = Color.white;
-    private static readonly Color capturedColor = new Color(0.24f, 0.71f, 1f);
+    private static readonly Color capturedColor = new Color(1f, 0.43f, 0.43f);
     private static readonly Color activeColor = new Color(0.8f, 0.8f, 0.8f);
-    private static readonly Color inactiveColor = new Color(0.5f, 0.5f, 0.5f);
+    private static readonly Color inactiveColor = new Color(0.45f, 0.45f, 0.45f);
     
     public List<Territory> neighbors;
     public LevelManager lm;
