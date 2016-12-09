923c5ae522c6312118d929c5ecbe637e49cab4a5
Alissa Hall <alissa.hall@utexas.edu>
2016-12-09 04:45:22 -0600

    adjusted credits scene

--- HexDX/Assets/Scripts/Miscellaneous/Credits.cs
+++ HexDX/Assets/Scripts/Miscellaneous/Credits.cs
@@ -3,22 +3,27 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Credits : MonoBehaviour {
-    private const float BASE_PERCENT_SCREEN = 3.5f; // rough percentage screen height base text takes to fully cross the screen
+    private const float BASE_PERCENT_SCREEN = 3.1f; // rough percentage screen height base text takes to fully cross the screen
     private const float PERCENT_SCREEN_PER_LINE = 0.0666f; // based on text size
 
     public Text text;
     public AudioSource audiosource;
     private float speed;
 
+    void Awake() {
+        speed = (BASE_PERCENT_SCREEN * Screen.height) / audiosource.clip.length ;
+    }
+
 	void Start () {
-        text.text += "\n";
-        if (GameManager.instance) {
+        if (GameManager.instance && GameManager.instance.deadUnitNames.Count > 0) {
+            text.text += "\n\nin loving memory of:\n\n";
             foreach (string name in GameManager.instance.deadUnitNames) {
                 text.text += name + "\n";
             }
-            speed = ((BASE_PERCENT_SCREEN + PERCENT_SCREEN_PER_LINE * GameManager.instance.deadUnitNames.Count) * Screen.height) / audiosource.clip.length ;
+            text.text += "\nwho did not make the perilous\njourney back home.\nthrough their noble sacrifice we shall carry on";
+            speed = ((BASE_PERCENT_SCREEN + PERCENT_SCREEN_PER_LINE * (GameManager.instance.deadUnitNames.Count + 7)) * Screen.height) / audiosource.clip.length ;
         }
-        text.text += "\nwho did not make the perilous \njourney back home. \n through their noble sacrifice we shall carry on\n\n\n\n made at the UT Austin GAMMA program\n\n\n\n\n\n\nTHANK YOU";
+        text.text += "\n\n\n\n made at the UT Austin GAMMA program\n\n\n\n\n\n\nTHANK YOU";
     }
 	
 	void Update () {
