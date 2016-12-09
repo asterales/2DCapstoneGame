a709421d31942e3d90adc5c6de32fe17ef578e6e
Anurag Papineni <anuragpapineni@gmail.com>
2016-12-09 02:09:41 -0600

    more balancing changes. also made commanders bigger than normal units

--- HexDX/Assets/Scripts/VictoryCondition/KillCommander.cs
+++ HexDX/Assets/Scripts/VictoryCondition/KillCommander.cs
@@ -1,7 +1,19 @@
-﻿public class KillCommander: VictoryCondition {
+﻿using UnityEngine;
+
+public class KillCommander: VictoryCondition {
     public Unit commander;
+    private bool resized = false;
     void Update()
     {
+        if (!resized && commander)
+        {
+            resized = true;
+            commander.transform.localScale = new UnityEngine.Vector3(1.4f, 1.4f, 1.0f);
+            GameObject g = Instantiate<GameObject>(Resources.Load<GameObject>("CommanderMark"));
+            g.transform.SetParent(commander.transform);
+            g.transform.localPosition = new Vector3(0, 0, 0.01f);
+            g.GetComponent<SpriteRenderer>().sortingOrder = 1;
+        }
         victoryConditionText.text = "kill the commander!";
     }
     public override bool Achieved() {
